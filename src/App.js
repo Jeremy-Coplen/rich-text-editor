a2bf4287136e6433cc8f5848bad7b09ce5486cf0
Jeremy Coplen <jeremycoplen.jc@gmail.com>
2020-12-21 15:22:30 -0700

    fixed initial errors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <p>rich text editor</p>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>rich text editor</p>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
